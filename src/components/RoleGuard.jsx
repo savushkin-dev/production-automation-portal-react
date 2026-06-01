d16388966491e19aa650932f7f6f8e80815e5993
NikolayPunko <punkon2001@gmail.com>
2026-06-01 16:00:36 +0300

    Добавлено обновление прав доступа после запроса на обновление токена доступа

--- src/components/RoleGuard.jsx
+++ src/components/RoleGuard.jsx
@@ -1,9 +1,14 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {Context} from '../index';
 import {observer} from 'mobx-react-lite';
 
 const RoleGuard = ({children, requiredRoles = [], fallback = null}) => {
     const {store} = useContext(Context);
+
+    useEffect(() => {
+        // При монтировании компонента и при изменении requiredRoles проверяем актуальные права
+        store.checkAuth();
+    }, [requiredRoles]);
 
     if (!store.isAuth) {
         return fallback;
